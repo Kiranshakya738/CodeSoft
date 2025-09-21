13ad219fca9405db8500b360dbcbf86205b85b6e
Kiran Shakya <kiranshakya652@gmail.com>
2025-09-21 22:02:23 +0530

    fifth commit

--- Task_03/ATMmachine.java
+++ Task_03/ATMmachine.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
+<<<<<<< HEAD
 
 // create bankaccount class to represent user's account
+=======
+>>>>>>> 3223258 (third commit)
 class BankAccount {
     private double balance;
     public BankAccount(double inbalance) {
@@ -33,7 +36,12 @@ class BankAccount {
 }
 
 
+<<<<<<< HEAD
 // create ATM class 
+=======
+// create ATM class to represent bankaccount
+
+>>>>>>> 3223258 (third commit)
 class ATM{
     Scanner sc = new Scanner(System.in);
     private BankAccount account;
@@ -43,8 +51,11 @@ class ATM{
         this.account = account;
     }
 
+<<<<<<< HEAD
 
     // design interface
+=======
+>>>>>>> 3223258 (third commit)
     public void start() {
         while (true) {
             System.out.println("welcome to the ATM machine!");
@@ -70,28 +81,38 @@ class ATM{
         }
     }
 
+<<<<<<< HEAD
     // method to take input and call withdraw method
+=======
+>>>>>>> 3223258 (third commit)
     public void wid() {
         System.out.print("Enter the amount: ");
         double amount = sc.nextDouble();
         account.withdraw(amount);
     }
 
+<<<<<<< HEAD
     // method to take input and call deposite method
+=======
+>>>>>>> 3223258 (third commit)
     public void dep() {
         System.out.print("Enter the amount: ");
         double amount = sc.nextDouble();
         account.deposit(amount);
     }
 
+<<<<<<< HEAD
 
     // method to call getbalance
+=======
+>>>>>>> 3223258 (third commit)
     public void checkBal() {
         System.out.println("Current Balance: "+account.getBalance());
     }
 
 }
 
+<<<<<<< HEAD
 
 
 // main class
@@ -104,6 +125,16 @@ public class ATMmachine {
         // create useraccount atm
         ATM atm = new ATM(userAccount);
         atm.start(); // start the process
+=======
+public class ATMmachine {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BankAccount userAccount = new BankAccount(10000);
+        ATM atm = new ATM(userAccount);
+        atm.start();
+>>>>>>> 3223258 (third commit)
 
     }
 
